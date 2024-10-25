610fac62dfcc991c8d482e574c10e258f43d64fb
TheShaylaMarie <shaylamariedesigns@gmail.com>
2024-10-24 19:20:26 -0500

    first vite commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Counter from './assets/components/Counter'
+import Greeting from './assets/components/Greeting'
+import "./greeting.css"
+
+function App() {
+
+  return (
+    <>
+      <Counter />
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/components/Greeting.jsx
@@ -0,0 +1,13 @@
+//component file names should be capitalized.
+
+import React from 'react'
+
+export default function Greeting(props) {
+
+
+  return (
+    <>
+    
+    </>
+  )
+}
